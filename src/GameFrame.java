d31acd48f627c977ca29ab8c6b0244148865e872
airkaras <132314500+airkaras@users.noreply.github.com>
2023-07-13 13:38:34 +0900

    GameFrame.java

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 public class GameFrame extends JFrame {
     public GameFrame(){
-        setTitle("2048");
+        setTitle("2048　コウショキ");
         setSize(370,420);
         getContentPane().setBackground(new Color(234,244,252));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
